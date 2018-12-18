1a0ff83af9cfb68e2126bf3f64896f4a9174be74
Serkan Özel <serkan.ozel@boun.edu.tr>
2018-12-18 19:18:12 +0300

    refactor comments

--- main.cpp
+++ main.cpp
@@ -3,78 +3,83 @@
 #include <fstream>
 #include <cassert>
 #include <cmath>
-#include <time.h>
+#include <ctime>
 #include <vector>
 
+//# of iterations of monte carlo process
 #define NUM_OF_ITERATIONS 5000000
 using namespace std;
 
-map<string,int> getAvailablePixels(int);
 
-int * localData = nullptr;
-int * noisyLocalData = nullptr;
-int* data = nullptr;
-int row_width;
+
+
+ map<string,int> getAvailablePixels(int);
+
+int * localData = nullptr; // Processors' local data
+int * noisyLocalData = nullptr; // Processors' local original image when they start processing(noisy)
+int* data = nullptr; // The noisy image read into this memory space in master processor
+int row_width; // This is the number of rows per processor. Note, (row_width) * (# of slave processors) = 200
 int main(int argc, char** argv) {
+    // comments will be written under or right of the code in this file except function definitions
 
-    // Initialize the MPI environment
     MPI_Init(&argc,&argv);
+    // Initialize the MPI environment
 
-    // Get the number of processes
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    // Get the number of processes
 
     row_width = 200/(world_size-1);
-    //A row's width
+    // Compute a row's width
 
-    // Get the rank of the process
     int world_rank;
-    // 0,1,2 ...
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    // Get the rank of the process
+    // Starts from 0
+
     if (argc != 5) {
         if(world_rank==0)
-            fprintf(stderr, "Usage: mpirun -n (# of processors) (inputfile) (outputfile) (beta) (pi)");
+            fprintf(stderr, "Usage: mpirun -n (# of processors) (executable) (inputfile) (outputfile) (beta) (pi)");
         exit(1);
     }
+    // Print information on how to use the program if argc is not 5.
 
     double beta;
     double pi;
     double gamma;
+    // Check out project description for explanation of these parameters.
 
-    // First get noisy image from input file to here. Also finally gather all local data to this.
-    if(world_rank==0){
-        data = new int[200*200];
-        // Allocate memory for image data to be used by master processor
+    if(world_rank==0) {
+        data = new int[200 * 200];
         assert(data != nullptr);
+        // Allocate memory for image data to be used by master processor and assert data is allocated
     }
-
-
-
-
-
-    // Local Data
-    if(world_rank!=0) {
+    else if(world_rank!=0) {
         localData = new int[row_width*200];
-        // allocate local memory for slave processors
         assert(localData != nullptr);
+        // allocate local memory for slave processors and assert if localData is allocated
     }
 
 
-    /*
-     * First, master processor reads from input file and scatters the data to all other processors
-     *
-     */
+     //  First, master processor reads from input file and scatters the data to all other processors
+
 
     if(world_rank==0){
+
         string inputFileName = argv[1];
         // get input file
+
         string outputFileName = argv[2];
         // get output file
+
         beta = stod(argv[3]);
         // get beta
+
         pi = stod(argv[4]);
         // get pi
+
         gamma = (1.0/2)*log((1-pi)/pi);
+        // compute gamma
 
         ifstream inputFile;
         inputFile.open(inputFileName);
@@ -87,38 +92,55 @@ int main(int argc, char** argv) {
                 data[200*i+j] = b;
             }
         }
+        // simply read from file into data 200x200 image
+
         inputFile.close();
+
+        // Now send data to slave ones
+
         for(int i= 0; i<world_size-1;i++){
             MPI_Send(&data[200*i*row_width],200*row_width,MPI_INT,i+1,0,MPI_COMM_WORLD);
         }
+        // send image data
+
         for(int i= 0; i<world_size-1;i++){
             MPI_Send(&beta,1,MPI_DOUBLE,i+1,1,MPI_COMM_WORLD);
         }
+        // send beta
+
         for(int i= 0; i<world_size-1;i++){
             MPI_Send(&pi,1,MPI_DOUBLE,i+1,2,MPI_COMM_WORLD);
         }
+        // send pi
+
         for(int i= 0; i<world_size-1;i++){
             MPI_Send(&gamma,1,MPI_DOUBLE,i+1,3,MPI_COMM_WORLD);
         }
+        // send gamma
+    }else{ // If I am a slave I expect information from the master
 
-    }else{
         MPI_Status dataStat;
         MPI_Status betaStat;
         MPI_Status piStat;
         MPI_Status gammaStat;
+        // Receiving statuses are stored here
+
         MPI_Recv(localData,200*row_width,MPI_INT,0,0,MPI_COMM_WORLD,&dataStat);
         MPI_Recv(&beta,1,MPI_DOUBLE,0,1,MPI_COMM_WORLD,&betaStat);
         MPI_Recv(&pi,1,MPI_DOUBLE,0,2,MPI_COMM_WORLD,&piStat);
         MPI_Recv(&gamma,1,MPI_DOUBLE,0,3,MPI_COMM_WORLD,&gammaStat);
+        // Note that tags were different above.
+
+
         noisyLocalData = new int[200*row_width];
         assert(noisyLocalData != nullptr);
         memcpy(localData,noisyLocalData, sizeof(int)*200*row_width);
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
+    // this barrier ensures all slave processors start processing when all of the data is distributed
+    if(world_rank!=0){        // start processing if I am the master
 
-    if(world_rank!=0){
-        // start processing
         srand(time(NULL));
         int selectedPixel = rand()%(200*row_width);
         int Xij = noisyLocalData[selectedPixel];
@@ -153,12 +175,21 @@ int main(int argc, char** argv) {
     }*/
 
 
-    // Finalize the MPI environment.
     delete [] data;
     delete [] localData;
     MPI_Finalize();
+    // Finalize the MPI environment and deallocate stuff.
 
 }
+/*  Function getAvailablePixels
+ *
+ * Def: Given index a pixel find which neighbors are available.
+ *
+ * parameter => index : int = the index of the pixel that we want to find neighbors of.
+ * return =>  map<string,int> : a map where keys
+ * are names like "top", "topRight","bottomLeft" and value are indexes of them.
+ *
+ */
 map<string,int> getAvailablePixels(int index){
     map<string,int> ret;
     if(!(index<200 && index>=0))ret["top"] = index - 200;
